a07ae2e1dc405f2818999c7e19a317a993be46e5
Warodom Khamphanchai <kwarodom@vt.edu>
2016-04-28 01:19:58 -0400

    add chat feature working

--- www/js/app.js
+++ www/js/app.js
@@ -78,4 +78,4 @@ app.config(function ($stateProvider, $urlRouterProvider) {
 		// if none of the above states are matched, use this as the fallback
 		$urlRouterProvider.otherwise('/');
 
-	});
+});
--- www/js/controllers/show.js
+++ www/js/controllers/show.js
@@ -3,12 +3,19 @@ var mod = angular.module('tvchat.controllers.show', []);
 
 
 mod.controller('ShowCtrl', function ($scope,
+                                     $stateParams,
+                                     ShowsService,
+                                     $firebaseArray,
                                      FIREBASE_URL,
+                                     $ionicScrollDelegate,
                                      UserService) {
 
 	$scope.user = UserService;
 
 	$scope.show = {};
+  $scope.showId = $stateParams.showId;
+  $scope.show = ShowsService.getShow(parseInt($scope.showId));
+  console.log($scope.show);
 
 	$scope.data = {
 		messages: [],
@@ -20,10 +27,37 @@ mod.controller('ShowCtrl', function ($scope,
 	var messagesRef = new Firebase(FIREBASE_URL);
 
 	$scope.loadMessages = function () {
+    console.log("Loading data for show ", $scope.show.name);
+
+    var query = messagesRef
+      .child("messages")
+      .orderByChild("showId")
+      .equalTo($scope.showId)
+      .limitToLast(200);
+
+    $scope.data.messages = $firebaseArray(query);
+    $scope.data.messages.$loaded().then(function(data){
+      console.log("AugularFire $loaded")
+      $scope.data.loading = false;
+      $ionicScrollDelegate.$getByHandle('show-page').scrollBottom(true);
+    });
 	};
 
-	$scope.sendMessage = function () {
-	};
+	$scope.sendMessage = function () { //send data to firebase
+    $scope.data.messages.$add({
+      showId: $scope.showId,
+      text: $scope.data.message,
+      username: $scope.user.current.name,
+      userId: $scope.user.current.userId,
+      profilePic: $scope.user.current.profilePic,
+      timestamp: new Date().getTime()
+    });
+    console.log("sent message "+$scope.data.message+" by user " +$scope.user.current.name+ " at "+ (new Date().getTime()))
+    $scope.data.message = '';
+    $ionicScrollDelegate.$getByHandle('show-page').scrollBottom(true);
+  };
+
+  $scope.loadMessages();
 
 	console.log("ShowCtrl-Created");
 
